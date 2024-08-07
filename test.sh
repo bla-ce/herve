6a138775be39ff75d7688b498c9bd9a602ba861f
bla-ce <contact.charaud@gmail.com>
2024-08-07 13:59:49 +1000

    trim parameters and anchor

--- test.sh
+++ test.sh
@@ -19,6 +19,9 @@ tests=(
   "localhost:1337/api/v1/ GET 200"
   "localhost:1337/api/v1/ PUT 404"
   "localhost:1337/not-found-not-found-not-found-not-found-not-found-not-found-not-found-not-found-not-found-not-found GET 400"
+  "localhost:1337/index?id=1 GET 200"
+  "localhost:1337/in?id=1 GET 404"
+  "localhost:1337/index#anchor GET 200"
 )
 
 total_tests=${#tests[@]}
