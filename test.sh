06c3fb801684ad5a567e58c4b3f4bebdbb93e113
bla-ce <contact.charaud@gmail.com>
2024-08-13 15:58:48 +1000

    remove method and route length, remove response
    remove response to add callback function

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No color
 # Define the tests - format: url method status_code (response)
 tests=(
   "localhost:1337/api GET 404"
-  "localhost:1337/health GET 200 ok"
+  "localhost:1337/health GET 200"
   "localhost:1337/ GET 200"
   "localhost:1337/index GET 200"
   "localhost:1337/ind GET 404"
@@ -35,21 +35,19 @@ perform_test() {
   local url=$1
   local method=$2
   local expected_status=$3
-  local expected_response=$4
-  local test_number=$5
-  local total_tests=$6
+  local test_number=$4
+  local total_tests=$5
 
   # Perform the request, capturing the HTTP status code and response body
   response=$(curl -s -w "\n%{http_code}" -X "$method" "$url")
   status_code=$(echo "$response" | tail -n1)
-  body=$(echo "$response" | sed '$d') # Removes the last line (status code)
 
   # Check if the actual status code matches the expected one
-  if [ "$status_code" -eq "$expected_status" ] && [[ "$body" == *"$expected_response"* ]]; then
-    echo -e "[${YELLOW}TEST $test_number/$total_tests${NC}] : $method $url -> ${GREEN}PASSED${NC} (expected: $expected_status, response: '$expected_response')"
+  if [ "$status_code" -eq "$expected_status" ]; then
+    echo -e "[${YELLOW}TEST $test_number/$total_tests${NC}] : $method $url -> ${GREEN}PASSED${NC} (expected: $expected_status)"
     passed_tests=$((passed_tests + 1))
   else
-    echo -e "[${YELLOW}TEST $test_number/$total_tests${NC}] : $method $url -> ${RED}FAILED${NC} (expected: $expected_status, got: $status_code, expected response: '$expected_response', got: '$body')"
+    echo -e "[${YELLOW}TEST $test_number/$total_tests${NC}] : $method $url -> ${RED}FAILED${NC} (expected: $expected_status, got: $status_code)"
   fi
 }
 
@@ -63,10 +61,9 @@ for test in "${tests[@]}"; do
   url="${parts[0]}"
   method="${parts[1]}"
   expected_status="${parts[2]}"
-  expected_response="${parts[3]-}"
 
   # Perform the test
-  perform_test "$url" "$method" "$expected_status" "$expected_response" "$test_number" "$total_tests"
+  perform_test "$url" "$method" "$expected_status" "$test_number" "$total_tests"
 
   # Increment the test number
   test_number=$((test_number + 1))
