f4f4fbc0e57a1ed2ce82133a4827b7f94dbb1035
bla-ce <contact.charaud@gmail.com>
2024-09-13 11:12:11 +1000

    send error response
    cannot parse now url too long

--- test.sh
+++ test.sh
@@ -23,7 +23,6 @@ tests=(
   "localhost:1337/i/v1/ GET 404"
   "localhost:1337/api/v1/ PUT 404"
   "localhost:1337/api/v1/ CON 405"
-  "localhost:1337/not-found-not-found-not-found-not-found-not-found GET 404"
   "localhost:1337/index?id=1 GET 200"
   "localhost:1337/index#anchor GET 200"
 )
