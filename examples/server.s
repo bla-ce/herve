cbaec65bc5a485192dc069fde0dc841c5ce46951
bla-ce <nicolas@woodfordia.com>
2025-02-09 15:04:16 +1000

    test response and close context

--- examples/server.s
+++ examples/server.s
@@ -3,133 +3,8 @@ global  _start
 %include "bytasm.inc"
 
 section .text
-print_hello:
-  lea   rdi, [hello]
-  mov   rsi, 0
-  call  println
-
-  ret
-
-cookie_func:
-  lea   rdi, [ck_test_name] 
-  call  is_cookie_name_valid
-  ret 
-
-header:
-  sub   rsp, 0x8
-
-  mov   [rsp], rdi
-
-  call  get_request_headers
-
-  mov   rdi, rax
-  mov   rsi, 0
-  call  println
-
-  lea   rdi, [name_param]
-  call  get_param
-
-  ; TODO: if failed?
-
-  lea   rdi, [rax]
-  mov   rsi, 0
-  call  println
-
-  lea   rdi, [STR_OK]
-  mov   rsi, 0
-  mov   rdx, 0
-  mov   rcx, [rsp]
-  call  send_response
-
-  add   rsp, 0x8
-
-  ret
-
-; @param rdi: request
-health:
-  sub   rsp, 0x8
-  mov   [rsp], rdi
-
-  call  get_headers
-  mov   [headers], rax
-
-  mov   rdi, [headers]
-  lea   rsi, [header_key]
-  lea   rdx, [header_value]
-  call  set_header
-
-  mov   rdi, [headers]
-  lea   rsi, [header_key]
-  lea   rdx, [header2_value]
-  call  set_header
-
-  mov   rdi, [headers]
-  lea   rsi, [header_key]
-  call  get_header_value
-  cmp   rax, 0
-  jl    .error
-
-  mov   rdi, rax
-  mov   rsi, 0
-  call  println
-
-  lea   rdi, [ok]
-  mov   rsi, [rsp]
-  call  serve_string
-  
-  mov   rax, SUCCESS_CODE
-  jmp   .return
-
-.error:
-  mov   rax, FAILURE_CODE
-
-.return:
-  add   rsp, 0x8
-  ret
-
-post:
-  sub   rsp, 0x8
-  
-  mov   [rsp], rdi
-
-  call  get_body
-  lea   rdi, [rax]
-  mov   rsi, 0
-  call  println
-
-  lea   rdi, [STR_OK]
-  mov   rsi, 0
-  mov   rdx, 0
-  mov   rcx, [rsp]
-  call  send_response
-
-  add   rsp, 0x8
-  ret
-
-index:
-  sub   rsp, 0x8
-
-  mov   rdi, [rsp]
-
-  lea   rdi, [index_path]
-  mov   rsi, [rsp]
-  call  serve_static_file
-
-  add   rsp, 0x8
-
-  ret
-
-send_200:
-  sub   rsp, 0x8
-  mov   [rsp], rdi
-
-  lea   rdi, [STR_OK]
-  mov   rsi, 0
-  mov   rdx, 0
-  mov   rcx, [rsp]
-  call  send_response
-
-  add   rsp, 0x8
+test_no_content:
+  call  send_no_content
   ret
 
 _start:
@@ -150,65 +25,16 @@ _start:
 
   mov   qword [sockfd], rax
 
+  ; add no content route
   mov   rdi, [rsp]
-  mov   rsi, print_hello
-  call  add_middleware
-
-  mov   rdi, [rsp]
-  mov   rsi, print_hello
-  call  add_middleware
-
-  mov   rdi, [rsp]
-  mov   rsi, print_hello
-  call  add_middleware
-
-  mov   rdi, [rsp]
-  lea   rsi, [POST]
-  lea   rdx, [post_route]
-  mov   rcx, post
+  lea   rsi, [GET]
+  lea   rdx, [root_url]
+  mov   rcx, test_no_content
   call  add_route
 
-  mov   rdi, [rsp]
-  lea   rsi, [GET]
-  lea   rdx, [v1_route]
-  mov   rcx, send_200
-  call  add_route 
+  cmp   rax, 0
+  jl    .error
 
-  mov   rdi, [rsp]
-  lea   rsi, [GET]
-  lea   rdx, [header_route]
-  mov   rcx, header
-  call  add_route 
-
-  mov   rdi, [rsp]
-  lea   rsi, [GET]
-  lea   rdx, [api_route]
-  mov   rcx, send_200
-  call  add_route 
-
-  mov   rdi, [rsp]
-  lea   rsi, [GET]
-  lea   rdx, [health_route]
-  mov   rcx, health
-  call  add_route 
-
-  mov   rdi, [rsp]
-  lea   rsi, [GET]
-  lea   rdx, [index_route]
-  mov   rcx, index
-  call  add_route
-
-  mov   rdi, [rsp]
-  lea   rsi, [GET]
-  lea   rdx, [cookie_route]
-  mov   rcx, cookie_func
-  call  add_route 
-
-  mov   rdi, [rsp]
-  lea   rsi, [views_dir]
-  mov   rdx, 1
-  call  add_dir_route
- 
   mov   rdi, [rsp]
   call  run_server
 
@@ -226,35 +52,9 @@ _start:
   syscall
 
 section .bss
-  headers resq 1
 
 section .data
   sockfd  dq 0
 
-  root_route    db "/", NULL_CHAR
-  health_route  db "/health", NULL_CHAR
-  post_route    db "/post", NULL_CHAR
-  index_route   db "/index", NULL_CHAR
-  header_route  db "/header", NULL_CHAR
-  cookie_route  db "/cookie", NULL_CHAR
-
-  api_route db "/api", NULL_CHAR
-  v1_route  db "/api/v1", NULL_CHAR
-
-  views_dir   db "examples/views", NULL_CHAR
-  index_path  db "examples/views/index.html", NULL_CHAR
-
-  ok  db "ok", NULL_CHAR
-
-  name_param db "name", NULL_CHAR
-
-  header_key    db "set-cookie", NULL_CHAR
-  header_value  db "value", NULL_CHAR
-  header2_value db "value2", NULL_CHAR
-
-  ck_test_name db "cookie=value", NULL_CHAR
-
-  hello db "Hello, World!", NULL_CHAR
-
-  log_file db "app.log", NULL_CHAR
+  root_url db "/", NULL_CHAR
 
