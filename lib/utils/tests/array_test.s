e450cfaba48f7ff13097f0d1d011de62ae030134
bla-ce <140779436+bla-ce@users.noreply.github.com>
2025-10-10 17:18:04 +0200

    array_insert pointers

--- lib/utils/tests/array_test.s
+++ lib/utils/tests/array_test.s
@@ -11,18 +11,54 @@ _start:
   ; *** STACK USAGE *** ;
   ; [rsp]   -> pointer to the array struct
 
-  mov   rdi, 10
+  mov   rdi, 4
   mov   rsi, 8
   call  array_create
   cmp   rax, 0
   jl    .error
 
-  mov   [rsp], rdi
+  mov   [rsp], rax
+
+  mov   rdi, [rsp]
+  mov   rsi, 420
+  call  array_push
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [rsp]
+  mov   rsi, 1024
+  call  array_push
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [rsp]
+  mov   rsi, -100
+  call  array_push
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [rsp]
+  mov   rsi, 50
+  call  array_push
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [rsp]
+  mov   rsi, 72
+  call  array_push
+  cmp   rax, 0
+  jl    .error
 
   mov   rdi, [rsp]
   call  array_free
   cmp   rax, 0
   jl    .error
+
+  mov   rdi, qword [mallocd]
+  mov   rsi, qword [freed]
+
+  cmp   rdi, rsi
+  jne   .error
 
   mov   rdi, SUCCESS_CODE
   call  exit
