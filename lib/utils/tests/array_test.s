061d9ed69538230d100f123e11b9204c746bb068
bla-ce <140779436+bla-ce@users.noreply.github.com>
2025-10-10 14:36:15 +0200

    array_free

--- lib/utils/tests/array_test.s
+++ lib/utils/tests/array_test.s
@@ -6,9 +6,21 @@ global _start
 section .text
 
 _start:
+  sub   rsp, 0x8
+
+  ; *** STACK USAGE *** ;
+  ; [rsp]   -> pointer to the array struct
+
   mov   rdi, 10
   mov   rsi, 8
   call  array_create
+  cmp   rax, 0
+  jl    .error
+
+  mov   [rsp], rdi
+
+  mov   rdi, [rsp]
+  call  array_free
   cmp   rax, 0
   jl    .error
 
