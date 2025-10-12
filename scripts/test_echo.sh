62ad9bc6c5315cb25b86511be895b68b7ba3ebc2
bla-ce <140779436+bla-ce@users.noreply.github.com>
2025-10-12 12:22:28 +0200

    add echo tests

---
+++ scripts/test_echo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+URL="http://localhost:8080/"
+WORDLIST="examples/echo/medium.txt"
+
+# check if the file exists
+if [ ! -f "$WORDLIST" ]; then
+  echo "File not found: $WORDLIST"
+fi
+
+# go over the list of words
+while IFS= read -r word;
+do
+  out=$(curl -s http://localhost:8080 --data-raw "${word}")
+
+  if [ "$out" == "$word" ]; then
+    echo "PASSED: $word"
+  else
+    echo "FAILED:"
+    echo "  Response: $out"
+    echo "  Expected: $word"
+    exit 1
+  fi
+done < "$WORDLIST"
