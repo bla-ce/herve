7bf9c7c1ce65cbceea00e2de95db9ea5291cd611
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-02-05 18:13:29 +0100

    no need for a specific init function

--- svc_impl/echo/echo_service.s
+++ svc_impl/echo/echo_service.s
@@ -1,7 +1,7 @@
 section .data
 
 echo_svc_t:
-  .id         db 0  ; set by the central server
+  .id         dq 0  ; set by the central server
   .name       dq 0  ; set by the caller
   .status     dq 0  ; set by the central server
   .port       dq 0  ; set by the central server
@@ -13,22 +13,31 @@ echo_svc_t:
   .next       dq 0  ; set by the central server
 echo_svc_t_end:
 
+echo_svc_msg:
+  .register db "Registering new echo server...", NULL_CHAR
+
 section .text
 
-; initialises a new echo service
-; @param  rdi: pointer to the service struct
+; registers a new echo service
+; @param  rdi: pointer to the context struct
+; @param  rsi: pointer to the service struct
 ; @return rax: return code
-echo_svc_init:
-  cmp   rdi, 0
-  jle    .error
+echo_svc_register:
+  ; get server from context
+  call  get_ctx_server
+  cmp   rax, 0
+  jl    .error
 
-  mov   qword [rdi+SERVICE_OFF_TYPE], SVC_TYPES.ECHO
-  mov   qword [rdi+SERVICE_OFF_REGISTER], echo_svc_register
-  mov   qword [rdi+SERVICE_OFF_UNREGISTER], echo_svc_unregister
-  mov   qword [rdi+SERVICE_OFF_START], echo_svc_start
-  mov   qword [rdi+SERVICE_OFF_STOP], echo_svc_stop
+  mov   rdi, rax
+  call  server_get_logger
+  cmp   rax, 0
+  jl    .error
 
-  mov   rax, SUCCESS_CODE
+  mov   rdi, rax
+  mov   rsi, echo_svc_msg.register
+  call  log_infoln
+  cmp   rax, 0
+  jl    .error
 
   jmp   .return
 
@@ -36,9 +45,6 @@ echo_svc_init:
   mov   rax, FAILURE_CODE
 
 .return:
-  ret
-
-echo_svc_register:
   ret
 
 echo_svc_unregister:
