188ad96da6e5cccc572181de8ccd4c5d28538667
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-02-05 17:43:01 +0100

    feat: echo_svc_init

--- svc_impl/echo/echo_service.s
+++ svc_impl/echo/echo_service.s
@@ -15,8 +15,29 @@ echo_svc_t_end:
 
 section .text
 
-; registers a new echo service
+; initialises a new echo service
 ; @param  rdi: pointer to the service struct
+; @return rax: return code
+echo_svc_init:
+  cmp   rdi, 0
+  jle    .error
+
+  mov   qword [rdi+SERVICE_OFF_TYPE], SVC_TYPES.ECHO
+  mov   qword [rdi+SERVICE_OFF_REGISTER], echo_svc_register
+  mov   qword [rdi+SERVICE_OFF_UNREGISTER], echo_svc_unregister
+  mov   qword [rdi+SERVICE_OFF_START], echo_svc_start
+  mov   qword [rdi+SERVICE_OFF_STOP], echo_svc_stop
+
+  mov   rax, SUCCESS_CODE
+
+  jmp   .return
+
+.error:
+  mov   rax, FAILURE_CODE
+
+.return:
+  ret
+
 echo_svc_register:
   ret
 
