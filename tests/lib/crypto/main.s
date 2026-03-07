991fbdb7f993792b29da9c0e13bea6e7159d65dd
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-03-07 13:34:50 +0100

    bcrypt disable padding

---
+++ tests/lib/crypto/main.s
@@ -0,0 +1,33 @@
+global _start
+
+%include "lib.inc"
+
+section .text
+_start:
+
+.loop:
+  call  _bcrypt_generate_salt
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, rax
+  call  println
+
+  dec   qword [counter]
+  cmp   qword [counter], 0
+  jle   .loop_end
+
+  jmp   .loop
+
+.loop_end:
+
+  mov   rdi, SUCCESS_CODE
+  call  exit
+
+.error:
+  mov   rdi, FAILURE_CODE
+  call  exit
+
+section .data
+
+counter dq 20
