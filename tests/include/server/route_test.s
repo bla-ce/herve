9d68981fa90265977d2d4e85df87ec81190a4c6a
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-03-14 16:56:47 +0100

    fix: route_create args

--- tests/include/server/route_test.s
+++ tests/include/server/route_test.s
@@ -7,12 +7,15 @@ section .data
 url_root    db "/", NULL_CHAR
 url_valid   db "/e", NULL_CHAR
 url_valid2  db "/hello", NULL_CHAR
+url_valid3  db "/user", NULL_CHAR
 
 url_without_leading_slash   db "e", NULL_CHAR
 url_without_leading_slash2  db "hello", NULL_CHAR
 
 url_with_trailing_slash   db "e/", NULL_CHAR
 url_with_trailing_slash2  db "hello/", NULL_CHAR
+
+url_with_all_slashes db "/user/", NULL_CHAR
 
 url_with_uppercase  db "HeLLo", NULL_CHAR
 
@@ -71,6 +74,15 @@ _start:
 
   mov   rdi, rax
   mov   rsi, url_valid2
+  call  assert_string_equal
+
+  mov   rdi, url_with_all_slashes
+  call  route_normalise_url
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, rax
+  mov   rsi, url_valid3
   call  assert_string_equal
 
   mov   rdi, SUCCESS_CODE
