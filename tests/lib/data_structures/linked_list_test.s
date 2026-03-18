ea2a6c63d2877f7d7b59cd63ecd899cfd8a804e1
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-03-18 15:36:15 +0100

    linked list public API

---
+++ tests/lib/data_structures/linked_list_test.s
@@ -0,0 +1,261 @@
+global _start
+
+%include "lib.inc"
+
+section .bss
+
+section .data
+
+head dq 0
+
+section .text
+
+_start:
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, 8
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 8
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_NEXT]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, 10
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 10
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rsi, [rax+NODE_OFF_NEXT]
+  mov   rdi, [rsi+NODE_OFF_DATA]
+  mov   rsi, 8
+  call  assert_equal
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [head]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, 8
+  call  linked_list_insert_at_end
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 8
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_NEXT]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, 10
+  call  linked_list_insert_at_end
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 8
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rsi, [rax+NODE_OFF_NEXT]
+  mov   rdi, [rsi+NODE_OFF_DATA]
+  mov   rsi, 10
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rsi, [rax+NODE_OFF_NEXT]
+  mov   rdi, [rsi+NODE_OFF_NEXT]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 10
+  call  assert_equal
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_end
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [head]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_end
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [head]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, 10
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, 8
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_delete_from_end
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 8
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_NEXT]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, 8
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, 14
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, 21
+  call  linked_list_insert_at_first
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 21
+  call  assert_equal
+
+  mov   rdi, head
+  mov   rsi, 14
+  mov   rdx, NO_ARG
+  call  linked_list_delete_from_value
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rsi, [rax+NODE_OFF_NEXT]
+  mov   rdi, [rsi+NODE_OFF_DATA]
+  mov   rsi, 8
+  call  assert_equal
+
+  mov   rdi, head
+  mov   rsi, 8
+  mov   rdx, NO_ARG
+  call  linked_list_delete_from_value
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, head
+  mov   rsi, 8
+  mov   rdx, NO_ARG
+  call  linked_list_delete_from_value
+  cmp   rax, 0
+  jl    .error
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_DATA]
+  mov   rsi, 21
+  call  assert_equal
+
+  mov   rax, [head]
+  mov   rdi, [rax+NODE_OFF_NEXT]
+  call  assert_is_zero
+
+  mov   rdi, head
+  mov   rsi, 21
+  mov   rdx, NO_ARG
+  call  linked_list_delete_from_value
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [head]
+  call  assert_is_zero
+
+  xor   r9, r9
+
+.loop:
+  cmp   r9, 20
+  jge   .loop_end
+
+  mov   rdi, head
+  mov   rsi, r9
+  call  linked_list_insert_at_end
+  cmp   rax, 0
+  jl    .error
+
+  inc   r9
+
+  jmp   .loop
+.loop_end:
+
+  mov   rdi, head
+  mov   rsi, NO_ARG
+  call  linked_list_free
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [mallocd]
+  cmp   [freed], rdi
+  jne   .error
+
+  mov   rdi, SUCCESS_CODE
+  call  exit
+
+.error:
+  mov   rdi, FAILURE_CODE
+  call  exit
