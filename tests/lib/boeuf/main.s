7f629ecd9c7bf655c50597d641b6007713954ce8
bla-ce <140779436+bla-ce@users.noreply.github.com>
2026-04-09 21:05:35 +0200

    boeuf: add more tests

--- tests/lib/boeuf/main.s
+++ tests/lib/boeuf/main.s
@@ -10,14 +10,15 @@ _start:
 
   mov   [boeuf_buf], rax
 
-  mov   rdi, rax
+  ; boeuf_is_corrupted returns FALSE for valid boeuf
+  mov   rdi, [boeuf_buf]
   call  boeuf_is_corrupted
 
   mov   rdi, rax
   mov   rsi, FALSE
   call  assert_equal
 
-  ; corrupt the boeuf to test
+  ; boeuf_is_corrupted returns TRUE for corrupted magic value
   mov   rax, [boeuf_buf]
   mov   qword [rax+_BOEUF_OFF_MAGIC_VALUE], 0xBEEF
 
@@ -28,21 +29,29 @@ _start:
   mov   rsi, TRUE
   call  assert_equal
 
+  ; restore magic value
   mov   rax, [boeuf_buf]
   mov   qword [rax+_BOEUF_OFF_MAGIC_VALUE], _BOEUF_MAGIC_VALUE
-
-  mov   rdi, rax
-  call  boeuf_get_max_cap
-
-  mov   rdi, rax
-  mov   rsi, _BOEUF_DEFAULT_MAX_CAPACITY
-  call  assert_equal
 
   mov   rdi, [boeuf_buf]
   mov   rsi, str_1
   call  boeuf_append_str
   cmp   rax, 0
   jl    .error
+
+  mov   rdi, [boeuf_buf]
+  call  boeuf_get_len
+
+  mov   rdi, rax
+  mov   rsi, 12 ; "Hello, sir! " is 12 chars
+  call  assert_equal
+
+  ; boeuf_get_cap is non-zero after append
+  mov   rdi, [boeuf_buf]
+  call  boeuf_get_cap
+
+  mov   rdi, rax
+  call  assert_is_not_zero
 
   mov   rdi, [boeuf_buf]
   call  boeuf_to_str
@@ -55,6 +64,42 @@ _start:
   mov   rsi, str_1
   call  assert_string_equal
 
+  mov   rdi, [boeuf_str]
+  call  free
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [boeuf_buf]
+  mov   rsi, str_2
+  call  boeuf_append_str
+  cmp   rax, 0
+  jl    .error
+
+  ; boeuf_get_len returns correct length after second append
+  mov   rdi, [boeuf_buf]
+  call  boeuf_get_len
+
+  mov   rdi, rax
+  mov   rsi, 24 ; "Hello, sir! How are you?" is 24 chars
+  call  assert_equal
+
+  mov   rdi, [boeuf_buf]
+  call  boeuf_to_str
+  cmp   rax, 0
+  jl    .error
+
+  mov   [boeuf_str], rax
+
+  mov   rdi, rax
+  mov   rsi, str_1_2
+  call  assert_string_equal
+
+  mov   rdi, [boeuf_str]
+  call  free
+  cmp   rax, 0
+  jl    .error
+
+  ; boeuf_clear resets length to 0
   mov   rdi, [boeuf_buf]
   call  boeuf_clear
   cmp   rax, 0
@@ -64,15 +109,100 @@ _start:
   mov   rdi, [rax+_BOEUF_OFF_LENGTH]
   call  assert_is_zero
 
+  ; boeuf_clear sets first byte to null
   mov   rax, [boeuf_buf]
   mov   rsi, [rax+_BOEUF_OFF_DATA]
   movzx rdi, byte [rsi]
   call  assert_is_zero
 
+  ; boeuf_get_cap is preserved after clear
+  mov   rdi, [boeuf_buf]
+  call  boeuf_get_cap
+
+  mov   rdi, rax
+  call  assert_is_not_zero
+
+  ; boeuf_append_str works after clear
+  mov   rdi, [boeuf_buf]
+  mov   rsi, str_2
+  call  boeuf_append_str
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [boeuf_buf]
+  call  boeuf_to_str
+  cmp   rax, 0
+  jl    .error
+
+  mov   [boeuf_str], rax
+
+  mov   rdi, rax
+  mov   rsi, str_2
+  call  assert_string_equal
+
   mov   rdi, [boeuf_str]
   call  free
   cmp   rax, 0
   jl    .error
+
+  mov   rdi, [boeuf_buf]
+  call  boeuf_clear
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [boeuf_buf]
+  mov   rsi, str_1
+  mov   rdx, 5 ; only "Hello"
+  call  boeuf_nappend
+  cmp   rax, 0
+  jl    .error
+
+  mov   rdi, [boeuf_buf]
+  call  boeuf_get_len
+
+  mov   rdi, rax
+  mov   rsi, 5
+  call  assert_equal
+
+  mov   rdi, [boeuf_buf]
+  call  boeuf_to_str
+  cmp   rax, 0
+  jl    .error
+
+  mov   [boeuf_str], rax
+
+  mov   rdi, rax
+  mov   rsi, str_hello
+  call  assert_string_equal
+
+  mov   rdi, [boeuf_str]
+  call  free
+  cmp   rax, 0
+  jl    .error
+
+  ; boeuf_nappend with null source returns success (no-op)
+  mov   rdi, [boeuf_buf]
+  mov   rsi, 0
+  mov   rdx, 10
+  call  boeuf_nappend
+
+  mov   rdi, rax
+  mov   rsi, SUCCESS_CODE
+  call  assert_equal
+
+  ; boeuf_nappend with zero length returns success (no-op)
+  mov   rdi, [boeuf_buf]
+  mov   rsi, str_1
+  mov   rdx, 0
+  call  boeuf_nappend
+
+  mov   rdi, rax
+  mov   rsi, SUCCESS_CODE
+  call  assert_equal
+
+  ; restore magic value for cleanup
+  mov   rax, [boeuf_buf]
+  mov   qword [rax+_BOEUF_OFF_MAGIC_VALUE], _BOEUF_MAGIC_VALUE
 
   mov   rdi, [boeuf_buf]
   call  boeuf_free
@@ -100,3 +230,5 @@ str_1     db "Hello, sir! ", NULL_CHAR
 str_2     db "How are you?", NULL_CHAR
 
 str_1_2   db "Hello, sir! How are you?", NULL_CHAR
+
+str_hello db "Hello", NULL_CHAR
